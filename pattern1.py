8d3ff1efbc3eb12869d3cdedcef0bcb496dabba9
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-04-21 19:54:55 +0530

    program

---
+++ pattern1.py
@@ -0,0 +1,40 @@
+for i in range(1,6):#1 2 3 4 5 6
+    for j in range(1,6):#21 22 23 24 25
+        print("*",end=" ")
+    print()
+    
+print("-----------------------------------------------")
+
+for i in range(1,6):
+    j=1
+    while j<6:
+        print("@",end=" ")
+        j=j+1
+    print()
+print("-------------------------------------------------")
+
+
+i=1
+while i<6:
+    for j in range(1,6):
+        print("#",end=" ")
+    i=i+1
+    print()
+
+print("-----------------------------------------")
+
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print("$",end=" ")
+        j=j+1
+    i=i+1
+    print()
+
+
+
+
+
+
+
---
+++ pattern2.py
@@ -0,0 +1,4 @@
+for i in range(65,70):#1
+    for j in range(i,70):
+        print(chr(i),end=" ")
+    print()
